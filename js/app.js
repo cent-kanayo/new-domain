cb8fd010e2d24395396d702978b416c3cb85bd05
cent kanayo <centkanayo@gmail.com>
2022-08-05 17:01:45 +0000

    Adding Layouts to the page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  $(window).on("load", function () {
+    $(".preloader").addClass("complete");
+  });
   $(window).on("scroll", function () {
     let scroll = $(window).scrollTop();
     if (scroll >= 50) {
@@ -7,4 +10,40 @@ $(document).ready(function () {
       $(".sticky").removeClass("sticky-add");
     }
   });
+
+  const type = new Typed(".element", {
+    strings: ["Chukwu Innocent", "a Developer", "a Designer", "a student"],
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000,
+  });
+
+  //   progress bars
+
+  const waypoint = new Waypoint({
+    element: document.getElementById("exp-id"),
+    handler: function () {
+      const p = document.querySelectorAll(".progress-bar");
+      p[0].setAttribute("style", "width:95%; transition: 1s all");
+      p[1].setAttribute("style", "width:80%; transition: 1.5s all");
+      p[2].setAttribute("style", "width:75%; transition: 1.7s all");
+      p[3].setAttribute("style", "width:65%; transition: 2s all");
+      p[4].setAttribute("style", "width:75%; transition: 2.3s all");
+      p[5].setAttribute("style", "width:70%; transition: 2.5s all");
+      p[6].setAttribute("style", "width:80%; transition: 2.7s all");
+    },
+    offset: "90%",
+  });
+
+  //   filters
+  const filterized = $(".filter-container").filterizr({
+    animationDuration: 0.5,
+  });
+
+  // copy date
+  const date = new Date().getFullYear();
+  $(".time").append(date);
 });
